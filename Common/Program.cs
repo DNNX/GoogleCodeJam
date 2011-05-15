058e0022c32e145b9ee5ba1915618aeb24f8ab6b
Viktar Basharymau <viktar.basharymau@gmail.com>
2011-05-15 22:20:46 +0300

    First commit

---
+++ Common/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Admin
+ * Date: 15.05.2011
+ * Time: 22:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Common
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
