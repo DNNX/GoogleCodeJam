e01c98508926bbd4052c920e0749b854dd908672
Viktar Basharymau <viktar.basharymau@gmail.com>
2011-05-16 08:55:03 +0300

    Added GoroSport

---
+++ GoroSport/GoroSport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using DNNX.GoogleCodeJam.Common;
+
+namespace DNNX.GoogleCodeJam.GoroSport
+{
+    public class GoroSport : Solution<int[]>
+    {
+        public GoroSport(TextReader input, TextWriter output)
+            : base(input, output)
+        {
+        }
+        
+        public GoroSport() {}
+        
+        public override object Solve(int[] a)
+        {
+            int count = 0;
+            for (int i=0; i<a.Length; ++i)
+                if (i+1 != a[i])
+                    ++count;
+            return count;
+        }
+        
+        public override int[] ReadTestCase()
+        {
+            ReadString();
+            return ReadInts();
+        }
+    
+        public static void Main(string[] args)
+        {
+            new GoroSport().REPL();
+            Console.ReadLine();
+        }    
+    }
+}
---
+++ GoroSport/GoroSportTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using DNNX.GoogleCodeJam.Common;
+
+namespace DNNX.GoogleCodeJam.GoroSport.Test
+{
+    [TestFixture]
+    public class GoroSportTest : BaseTest<int[]>
+    {
+        [Test]
+        public void SmokeTest()
+        {
+            var input = @"
+3
+ 2
+ 2 1
+ 3
+ 1 3 2
+ 4
+ 2 1 4 3";
+            
+            var expectedOutput = @"
+Case #1: 2
+Case #2: 2
+Case #3: 4";
+
+            SmokeTest(input, expectedOutput);
+        }
+        
+        [Test]
+        public void EdgeCase()
+        {
+            Assert.AreEqual(4, CreateSolution().Solve(new[] {1, 2, 5, 4, 3, 7, 6}));
+        }
+        
+        
+        protected override Solution<int[]> CreateSolution(TextReader input, TextWriter output)
+        {
+            return new GoroSport(input, output);
+        }
+    }
+}
